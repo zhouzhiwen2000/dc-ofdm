d55e2e2eed77afc0d5b23f6e5d854e0a75645603
ncotti <ncotti@frba.utn.edu.ar>
2024-06-26 16:59:03 -0300

    Generate HDL for Frequency Upshift
    There was a bug with the NCO, where it needed an accumulator word length greater than 3 (previous value was 3).

--- inc/constants.m
+++ inc/constants.m
@@ -86,7 +86,10 @@ interpolatorDelay = mean(grpdelay(interpolatorFilter));
 ncoPhaseStep = 1/(2*oversamplingFactor);
 
 % The NCO will have a phase resolution of 1/2^ncoWordLength.
-ncoWordLength = ceil(log2(2*oversamplingFactor)+1);
+ncoWordLength = ceil(log2(2*oversamplingFactor)+2);
+if (ncoWordLength < 4)
+    error("ncoWordLength must be greater than 4 (or HDL code can't be generated)");
+end
 
 % The phase increment takes a value from [0; 2^ncoWordLength]
 % A phase increment of "2^ncoWordLength" is equal to 2pi
