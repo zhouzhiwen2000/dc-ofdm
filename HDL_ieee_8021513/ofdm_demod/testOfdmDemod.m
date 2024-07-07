78040631aac2f56bef0072732671906c4fcf16b9
ncotti <ncotti@frba.utn.edu.ar>
2024-07-07 16:55:13 -0300

    Add HDL ofdm demodulator

---
+++ HDL_ieee_8021513/ofdm_demod/testOfdmDemod.m
@@ -0,0 +1,66 @@
+%% Test OFDM Demod
+clc; clear; close all;
+addpath("../../src");
+addpath("../../inc");
+addpath("../../src/rx");
+constants;
+
+%% Inputs
+cpLen = 16;
+nSym = 5;
+
+input = rand(numDataCarriers, nSym) + 1i*rand(numDataCarriers, nSym);
+dataSymbols = ofdmmod(input, N, cpLen, nullIdx);
+validIn = true(length(dataSymbols), 1);
+dataSymbols = dataSymbols(:);
+
+expectedOut = input;
+
+%% Simulation Time
+latency = 3000/fs;         % Algorithm latency. Delay between input and output
+stopTime = (length(validIn)-1)/fs + latency;
+
+%% Run the simulation
+model_name = "HDLOfdmDemod";
+
+load_system(model_name);
+simOut = sim(model_name);
+
+dataOut = get(simOut, "dataOut");
+startOut = get(simOut, "startOut");
+endOut = get(simOut, "endOut");
+validOut = get(simOut, "validOut");
+
+%% Compare with MATLAB reference algorithm
+
+startIdx = find(startOut == true);
+endIdx = find(endOut == true);
+
+assert(isequal(length(startIdx), length(endIdx)), ...
+    "Length of start and end should be the same.");
+
+for i=1:length(startIdx)
+    out = dataOut(startIdx(i):endIdx(i));
+    assert(iskindaequal(expectedOut(:,i), out, 1e-3), "OFDM output is not the same");
+    assert(sum(validOut(startIdx(i):endIdx(i)) == 0) == 0);
+end
+
+%% Plotting
+n = 1:1:length(out);
+
+figure();
+subplot(2,1,1)
+plot(n, abs(out), n, abs(expectedOut(:,nSym)));
+legend("Out", "ExpectedOut");
+xlabel("n [samples]");
+xlim([n(1), n(end)]);
+grid on;
+
+subplot(2,1,2)
+plot(n, abs(out - expectedOut(:,nSym)));
+xlabel("n [samples]");
+title("|out - expectedOut|");
+xlim([n(1), n(end)]);
+grid on;
+
+disp("Test successfull!");
