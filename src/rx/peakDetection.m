b9acb9a1e6fcad0957bbf9e98baea4ad8e3e7e8e
ncotti <ncotti@frba.utn.edu.ar>
2024-07-21 08:50:23 -0300

    Add unit test for peak detection

---
+++ src/rx/peakDetection.m
@@ -0,0 +1,35 @@
+function [peaks] = peakDetection(dataIn, k, threshold)
+%PEAKDETECTION Detect a local peak.
+%   Detects a local peak within a "2*K" sample window. Value is considered
+%   a peak only if "s1(i) > threshold"
+arguments(Input)
+    dataIn (:,1) double
+    k double
+    threshold double
+end
+arguments(Output)
+    peaks (:,1) double
+end
+    s1 = zeros(length(dataIn), 1);
+    for i=1+k:1:length(dataIn)-k
+        s1(i) = ( min(dataIn(i-k:i-1)) + min(dataIn(i+1:i+k)) ) /2;
+        %s2(i) = dataIn(i) - (sum(dataIn(i-k:i-1)) + sum(dataIn(i+1:i+k)))/2;
+    end
+
+    indexes = find(s1 > threshold);
+
+    newIndexes = 1;
+    newIndexesLen = 1;
+    for i=1:1:length(indexes)-1
+        if (indexes(i+1) - indexes(i) <= k)
+            if (dataIn(indexes(i)) > dataIn(newIndexes(newIndexesLen)))
+                newIndexes(newIndexesLen) = indexes(i);
+            end
+        else
+            newIndexesLen = newIndexesLen + 1;
+            newIndexes(newIndexesLen) = indexes(i);
+        end
+    end
+    peaks = newIndexes;
+end
+
---
+++ unitTest/testPeakDetection.m
@@ -0,0 +1,49 @@
+%% Test Peak Detection
+% Implementation of the algorith of local peak detection in the paper
+% "Simple Algorithms for Peak Detection in Time-Series"
+clc; clear; close all;
+addpath("../src");
+addpath("../src/rx");
+addpath("../inc");
+
+%% Create input data
+%%% Generate a random signal with peaks
+inputLen = 2000;        % Size of input signal
+peakLen = 58;           % Length of the gaussian pulse, which is used as a peak        
+peakThreshold = 0.5;    % A peak will be considered only if it's value is greater than this threshold
+k = 8;                  % Window size for peak detection
+
+% Location of peaks in the input data
+peakPosition = [200 600 1500]';
+
+%%% Signal generation
+dataIn = zeros(inputLen, 1);
+peak = (peakLen/3*gaussdesign(1,1,peakLen)).';
+
+for i=1:1:length(peakPosition)
+    dataIn(peakPosition(i)-peakLen/2 : peakPosition(i) + peakLen/2) = ...
+        dataIn(peakPosition(i)-peakLen/2 : peakPosition(i) + peakLen/2) + peak;
+end
+
+%%% Add some false peaks
+falsePeakPosition = randi([peakLen/2+1, length(dataIn)-peakLen/2], 30, 1);
+falsePeak = peak/3;
+
+for i=1:1:length(falsePeakPosition)
+    dataIn(falsePeakPosition(i)-peakLen/2 : falsePeakPosition(i) + peakLen/2) = ...
+        dataIn(falsePeakPosition(i)-peakLen/2 : falsePeakPosition(i) + peakLen/2) + falsePeak;
+end
+
+dataIn = awgn(dataIn, 20);
+
+%% Outputs and expected outputs
+out = peakDetection(dataIn, k, 0.5);
+
+%% Test
+% Don't assert, because the noise might move the peaks position
+%assert(isequal(out, peakPosition));
+
+plot(dataIn)
+
+disp("Check in the graph if the out peaks are correct");
+out
