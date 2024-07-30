9ff3079e376261733fd4eff845084d4a9406c30f
ncotti <ncotti@frba.utn.edu.ar>
2024-07-29 22:23:08 -0300

    Add algorithm test with image transmission

---
+++ integrationTest/simTxRx/simImageTxRx.m
@@ -0,0 +1,22 @@
+%% Test transmission and reception of an image
+% The image is transmitted in raw RGB bits.
+% Warning: test can take several minutes to run!
+clc; clear; close all;
+addpath("../../src");
+addpath("../../inc");
+addpath("../../src/rx");
+constants;
+
+parametersFile = "parameters.m";
+imageFile = "test_image.png";
+
+msg = imread(imageFile);
+[pBits, imageH, imageW, imageC] = img2binl(imageFile);
+
+OFDMSignal = fullTx(parametersFile, pBits);
+pBitsRx = fullRx(OFDMSignal);
+
+msgRx = binl2img(pBitsRx, imageH, imageW, imageC);
+assert(isequal(msg, msgRx));
+
+disp("Test Successfull!");
