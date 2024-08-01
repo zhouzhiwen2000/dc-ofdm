64ea4dddf828c805bb8d19346cebd2b5680130f3
ncotti <ncotti@frba.utn.edu.ar>
2024-08-01 00:52:02 -0300

    Add HDLOFDMSS refined
    There are no bugs now, it's fully operational

--- HDL_ieee_8021513/ofdm_ss/testOFDMSS.m
+++ HDL_ieee_8021513/ofdm_ss/testOFDMSS.m
@@ -11,17 +11,14 @@ SNR = 40;
 
 preambleTx = ofdmModulate(preambleOFDMSymbols, preambleBitsPerSubcarrier, preambleCyclicPrefixLen, nullIdx, preambleScramblerInit);
 
-energy = 1/sqrt(2)*abs(mean(preambleTx));
-
-noise = energy*rand(delay, 1) + 1i*energy*rand(delay, 1);
-
-dataIn = [noise; preambleTx; noise;];
-dataIn = awgn(dataIn, SNR);     % Add noise
+% Add noise
+dataIn = [zeros(delay, 1); preambleTx; zeros(delay, 1)];
+dataIn = awgn(dataIn, SNR);     
 
 validIn = true(size(dataIn));
 
-[~, expectedDelayOut, expectedOut] = ofdmSymbolSync(dataIn);
-expectedDelayOut
+% Expected output
+[~, expectedDelayOut, expectedMOut, expectedPeaksOut] = ofdmSymbolSync(dataIn);
 
 %% Simulation Time
 latency = (delay+300)/fs;         % Algorithm latency. Delay between input and output
@@ -34,6 +31,7 @@ load_system(model_name);
 simOut = sim(model_name);
 
 dataOut = get(simOut, "dataOut");
+mOut = get(simOut, "mOut");
 startOut = get(simOut, "startOut");
 endOut = get(simOut, "endOut");
 validOut = get(simOut, "validOut");
@@ -41,30 +39,39 @@ peakOut = get(simOut, "peakOut");
 indexOut = get(simOut, "indexOut");
 
 %% Compare with MATLAB reference algorithm
+% Compare first the function "M(d)"
 startIdx = find(startOut == true);
 endIdx = find(endOut == true);
 
 assert(isequal(length(startIdx), length(endIdx)), ...
     "Length of start and end should be the same.");
 
+% Test that M(d) is equal
 for i=1:length(startIdx)
-    out = dataOut(startIdx(i):endIdx(i));
-    assert(iskindaequal(expectedOut, out, 10e-3));
+    mOutSim = mOut(startIdx(i):endIdx(i));
+    assert(iskindaequal(expectedMOut, mOutSim, 10e-3));
     assert(sum(validOut(startIdx(i):endIdx(i)) == 0) == 0);
 end
 
-% The delay for the OFDM signal is the first detected peak
-indexes = find(peakOut ~= 0);
-delayOut = indexOut(indexes(2)) % TODO, index 1 below 100? Why
+% Test that the peaks detected are equal
+peaksSim = find(peakOut ~= 0);
+peaksSim = indexOut(peaksSim);
+assert(isequal(peaksSim, expectedPeaksOut));
 
-assert(iskindaequal(delayOut, expectedDelayOut, 10));
+% Test that the signal at the output is only the preamble
+startPreamble = find(peakOut == true);
+startPreamble = startPreamble(1);
+preambleWithNoise = dataIn(delay+1:delay+length(preambleTx));
+preambleSim = dataOut(startPreamble:startPreamble-1+length(preambleTx));
+assert(iskindaequal(preambleSim, preambleWithNoise, 1e-3));
+
 
 %% Plotting
 figure();
-n = 0:1:length(out)-1;
+n = 0:1:length(mOutSim)-1;
 
 subplot(2,1,1);
-plot(n, real(out), n, real(expectedOut));
+plot(n, real(mOutSim), n, real(expectedMOut));
 legend("Out", "ExpectedOut");
 xlabel("n [samples]");
 ylabel("M(d)");
@@ -73,7 +80,7 @@ xlim([min(n), max(n)]);
 grid on;
 
 subplot(2,1,2);
-plot(n, abs(out-expectedOut));
+plot(n, abs(mOutSim-expectedMOut));
 xlabel("n [samples]");
 ylabel("Error");
 title("|out - expectedOut|");
